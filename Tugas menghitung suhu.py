b7569513b42975aa8122b740342d98f6f8e13c42
ALVEN <143860034+Mfajrialfaini@users.noreply.github.com>
2023-09-08 15:08:19 +0700

    Add files via upload

---
+++ Tugas menghitung suhu.py
@@ -0,0 +1,20 @@
+print("Membuat penghitungan suhu")
+print("Muhammad fajri alfaini")
+
+     #Soal no 1 celcius menjadi fahrenheit
+#celcius = float(input('masukan suhu dalam celsius : '))
+#print("suhu adalah",celcius, "celcius")
+#fahrenheit = ((9/5) * celcius) + 32
+#print("suhu adalah", fahrenheit, "fahrenheit")
+
+     #Soal no 2 fahrenheit menjadi celcius
+#fahrenheit = float(input('masukan suhu'))
+#print('suhu adalah', fahrenheit, "fahrenheit")
+#celcius = (5/9*(fahrenheit-32))
+#print("suhu adalah",celcius, "celcius")
+
+     #Soal no 3 celcius menjadi kelvin
+#celcius = float(input('masukan suhu dalam celsius :'))
+#print('suhu adalah', celcius, "celcius")
+#kelvin = celcius + 273
+#print("suhu adalah", kelvin, "kelvin")
